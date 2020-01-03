dbe749280ba5970374c4019f54688bb536b0c0ae
Darvey <darvey6@hotmail.com>
2020-01-03 02:47:56 -0700

    Updated db schema with user details for frontend redux data

--- dbschema.js
+++ dbschema.js
@@ -22,4 +22,31 @@ let db = {
 
         }
     ]
-}
+};
+
+const userDetails = {
+    // Redux data
+    // what user information is hold in our redux state in front end. Used to populate our profile with.
+
+    credentials: {
+      userId: 'N43KJ5H43KJHREW4J5H3JWMERHB',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'Lonodn, UK'
+    },
+    //need to check if posts are liked by User and to show different icon if it is/isnt
+    likes: [
+      {
+        userHandle: 'user',
+        screamId: 'hh7O5oWfWucVzGbHH2pa'
+      },
+      {
+        userHandle: 'user',
+        screamId: '3IOnFoQexRcofs5OhBXO'
+      }
+    ]
+  };
